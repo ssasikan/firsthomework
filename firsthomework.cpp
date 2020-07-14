030d96bf4ff6169139ca06e45ba9e097f67f5647
ssasikan <68280293+ssasikan@users.noreply.github.com>
2020-07-14 18:43:29 +0700

    Add files via upload

---
+++ firsthomework.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	std::cout <<"       #       "<< endl;
+	std::cout <<"      # #      "<< endl;
+	std::cout <<"     #   #       "<< endl;
+	std::cout <<"    #     #       "<< endl;
+	std::cout <<"   # ##### #      "<< endl;
+	std::cout <<"  #         #       "<< endl;
+	std::cout <<"sasikarn lalab"<< endl;
+	return 0;
+}
